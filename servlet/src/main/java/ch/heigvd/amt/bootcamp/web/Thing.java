577c3a0fbd18fb2f0c50c4d01bec1e37c1d2f1a7
Christopher Meier <christopher.meier@heig-vd.ch>
2017-09-23 22:07:37 +0200

    Initial template and MVC structure.

---
+++ servlet/src/main/java/ch/heigvd/amt/bootcamp/web/Thing.java
@@ -0,0 +1,27 @@
+package ch.heigvd.amt.bootcamp.web;
+
+public class Thing {
+    private String title;
+    private String description;
+
+    public Thing(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
