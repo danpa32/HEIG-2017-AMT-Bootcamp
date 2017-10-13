4259c7cf27eeee4380f322a1d0595fabbbdc72bd
Daniel Palumbo <dpalumbo06@gmail.com>
2017-10-13 15:46:37 +0200

    create the database and table associate with docker

--- servlet/images/mysql/data/database.sql
+++ servlet/images/mysql/data/database.sql
@@ -5,3 +5,13 @@ CREATE USER IF NOT EXISTS 'admin'@'%' IDENTIFIED BY 'adminpw';
 GRANT ALL PRIVILEGES ON db_quotes.* TO 'admin'@'%';
 
 USE db_quotes;
+
+CREATE TABLE IF NOT EXISTS quote (
+	id INT(4) NOT NULL AUTO_INCREMENT,
+	quote TEXT NOT NULL,
+	author VARCHAR(50) NOT NULL,
+	category VARCHAR(30),
+	source VARCHAR(40) NOT NULL,
+	date DATE NOT NULL,
+	PRIMARY KEY (id)
+);
