2c0a5f00a39dce3779e9da3f4b4d536e8880be94
Christopher Meier <christopher.meier@heig-vd.ch>
2017-10-15 19:39:52 +0200

    Conf to delete all quotes done.

--- servlet/src/main/java/ch/heigvd/amt/bootcamp/web/ConfigurationServlet.java
+++ servlet/src/main/java/ch/heigvd/amt/bootcamp/web/ConfigurationServlet.java
@@ -22,25 +22,45 @@ public class ConfigurationServlet extends HttpServlet {
     AlertManagerLocal alertManager;
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String nbThingParam = request.getParameter("nbThings");
-        if (nbThingParam != null) {
-            try {
-                int nbThings = Integer.parseInt(nbThingParam);
+        String confParam = request.getParameter("conf");
+        if(confParam != null) {
+            switch (confParam) {
+                case "gen":
+                    String nbThingParam = request.getParameter("nbThings");
+                    if (nbThingParam != null) {
+                        try {
+                            int nbThings = Integer.parseInt(nbThingParam);
 
-                boolean success = quotesManager.generateQuotes(nbThings);
+                            boolean success = quotesManager.generateQuotes(nbThings);
 
-                if (success) {
-                    alertManager.add(request, new Alert(Alert.Level.SUCCESS, "Success", "The generation of " + nbThings + " quotes has succeeded"));
-                } else {
-                    alertManager.add(request, new Alert(Alert.Level.WARNING, "Failed", "The generation of " + nbThings + " quotes has failed"));
-                }
+                            if (success) {
+                                alertManager.add(request, new Alert(Alert.Level.SUCCESS, "Success", "The generation of " + nbThings + " quotes has succeeded."));
+                            } else {
+                                alertManager.add(request, new Alert(Alert.Level.WARNING, "Failed", "The generation of " + nbThings + " quotes has failed."));
+                            }
 
-                request.setAttribute("nbGeneratedQuotes", request.getParameter("nbGeneratedQuotes"));
-            } catch (NumberFormatException nfe) {
-                alertManager.add(request, new Alert(Alert.Level.DANGER, "Incorrect parameter", "Couldn't parse parameter"));
+                            request.setAttribute("nbGeneratedQuotes", request.getParameter("nbGeneratedQuotes"));
+                        } catch (NumberFormatException nfe) {
+                            alertManager.add(request, new Alert(Alert.Level.DANGER, "Incorrect parameter", "Couldn't parse parameter."));
+                        }
+                    } else {
+                        alertManager.add(request, new Alert(Alert.Level.DANGER, "Missing parameter", "Couldn't find the number of quotes to generate."));
+                    }
+                    break;
+                case "del":
+                    String delConfParam = request.getParameter("confDelete");
+                    if(delConfParam != null && delConfParam.equals("on")) {
+                        quotesManager.deleteAllQuotes();
+                        alertManager.add(request, new Alert(Alert.Level.SUCCESS, "Success", "The deletions of all quotes has succeeded."));
+                    } else {
+                        alertManager.add(request, new Alert(Alert.Level.WARNING, "Missing confirmation", "You must check the confirmation checkbox to delete all quotes."));
+                    }
+                    break;
+                default:
+                    alertManager.add(request, new Alert(Alert.Level.WARNING, "Unknown configuration", "Unknown configuration to do."));
             }
         } else {
-            alertManager.add(request, new Alert(Alert.Level.DANGER, "Missing parameter", "Couldn't find the number of quotes to generate"));
+            alertManager.add(request, new Alert(Alert.Level.WARNING, "Malformed request", "Couldn't find which configuration to do."));
         }
 
         request.getRequestDispatcher("/WEB-INF/pages/configuration.jsp").forward(request, response);
--- servlet/src/main/webapp/WEB-INF/pages/configuration.jsp
+++ servlet/src/main/webapp/WEB-INF/pages/configuration.jsp
@@ -9,18 +9,55 @@
 <%@include file="includes/header.jsp"%>
 
 <div class="container">
-    <form class="my-4" role="form" action="./configuration" method="post">
-        <fieldset>
-            <div class="form-group col-md-4">
-                <label for="nbThings">Number of things</label>
-                <input class="form-control" name="nbThings" id="nbThings" placeholder="Number of things to generate" type="text">
-            </div>
-
-            <div class="col-md-4">
-                <button type="submit" class="btn btn-primary float-right">Generate</button>
-            </div>
-        </fieldset>
-    </form>
+    <legend class="mt-4">Configuration</legend>
+    <div class="row my-4">
+        <div class="col-sm-6">
+            <form class="card" role="form" action="<c:url value="/configuration"/>" method="post">
+                <div class="card-header">
+                    Quotes generation
+                </div>
+                <div class="card-body">
+                    <input name="conf" id="confGen" value="gen" hidden type="text">
+                    <fieldset>
+                        <div class="form-group">
+                            <label for="nbThings">Number of quotes</label>
+                            <input class="form-control" name="nbThings" id="nbThings" placeholder="Number of quotes to generate" type="text">
+                        </div>
+                    </fieldset>
+                </div>
+                <div class="card-footer">
+                    <div class="col-ml-4">
+                        <button type="submit" class="btn btn-primary float-right">Generate</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+        <div class="col-sm-6">
+            <form class="card" role="form" action="<c:url value="/configuration"/>" method="post">
+                <div class="card-header">
+                    Quotes deletion
+                </div>
+                <div class="card-body">
+                    <input name="conf" id="confDel" value="del" hidden type="text">
+                    <fieldset>
+                        <div class="form-check">
+                            <label class="form-check-label">
+                                <input class="form-check-input" type="checkbox" name="confDelete" onload="function () {
+                                  this.checked = false;
+                                }">
+                                Are you sure you want to delete all the quotes in the database ?
+                            </label>
+                        </div>
+                    </fieldset>
+                </div>
+                <div class="card-footer">
+                    <div class="col-ml-4">
+                        <button type="submit" class="btn btn-danger float-right">Delete all</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
 </div>
 
 <%@include file="includes/footer.jsp"%>
